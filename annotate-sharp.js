68240f61cb774c471f89fbfd85376a3e0789d81d
smcgann99 <52132596+smcgann99@users.noreply.github.com>
2025-01-28 02:27:22 +0000

    updates

--- annotate-sharp.js
+++ annotate-sharp.js
@@ -1,17 +1,18 @@
 module.exports = function(RED) {
     "use strict";
     const sharp = require("sharp");
-   
+    const path = require("path");
+
     function AnnotateSharpNode(config) {
         RED.nodes.createNode(this, config);
         this.data       = config.data || "";
         this.dataType   = config.dataType || "msg";
         var node = this;
-        const defaultFill = config.fill || "";
         const defaultStroke = config.stroke || "#ffC000";
         const defaultLineWidth = parseInt(config.lineWidth) || 5;
         const defaultFontSize = config.fontSize || 24;
         const defaultFontColor = config.fontColor || "#ffC000";
+        const fontPath = path.join(__dirname, "SourceSansPro-Regular.ttf");
         let input = null;
 
         this.on("input", function(msg) {
@@ -31,8 +32,11 @@ module.exports = function(RED) {
                         .metadata()
                         .then(metadata => {
                             let image = sharp(buffer);
+                            let svgAnnotations = '<svg width="' + metadata.width + '" height="' + metadata.height + '">';
+                            svgAnnotations += `<style>@font-face { font-family: 'SourceSansPro'; src: url('${fontPath}'); }</style>`;
+
                             msg.annotations.forEach(function(annotation) {
-                                let x, y, r, w, h;
+                                let x, y, r, w, h, textX, textY;
 
                                 if (!annotation.type && annotation.bbox) {
                                     annotation.type = 'rect';
@@ -51,39 +55,45 @@ module.exports = function(RED) {
                                             w = annotation.w;
                                             h = annotation.h;
                                         }
-                                        image = image.composite([{
-                                            input: Buffer.from(
-                                                `<svg>
-                                                    <rect x="${x}" y="${y}" width="${w}" height="${h}" 
-                                                    fill="${annotation.fill || defaultFill}" 
-                                                    stroke="${annotation.stroke || defaultStroke}" 
-                                                    stroke-width="${annotation.lineWidth || defaultLineWidth}" />
-                                                </svg>`
-                                            ),
-                                            top: 0,
-                                            left: 0
-                                        }]);
+                                        if (x < 0) {
+                                            w += x;
+                                            x = 0;
+                                        }
+                                        if (y < 0) {
+                                            h += y;
+                                            y = 0;
+                                        }
+                                        svgAnnotations += `<rect x="${x}" y="${y}" width="${w}" height="${h}" 
+                                                            fill="none" 
+                                                            stroke="${annotation.stroke || defaultStroke}" 
+                                                            stroke-width="${annotation.lineWidth || defaultLineWidth}" />`;
+                                        if (annotation.label) {
+                                            textY = (y - 5 < 0 || y - 5 < metadata.height - (y + h + 5 + (annotation.fontSize || defaultFontSize))) ? y + h + 5 + (annotation.fontSize || defaultFontSize) : y - 5;
+                                            svgAnnotations += `<text x="${x}" y="${textY}" font-size="${annotation.fontSize || defaultFontSize}" 
+                                                                fill="${annotation.fontColor || defaultFontColor}" font-family="SourceSansPro">${annotation.label}</text>`;
+                                        }
                                         break;
                                     case 'circle':
                                         x = annotation.x;
                                         y = annotation.y;
                                         r = annotation.r;
-                                        image = image.composite([{
-                                            input: Buffer.from(
-                                                `<svg>
-                                                    <circle cx="${x}" cy="${y}" r="${r}" 
-                                                    fill="${annotation.fill || defaultFill}" 
-                                                    stroke="${annotation.stroke || defaultStroke}" 
-                                                    stroke-width="${annotation.lineWidth || defaultLineWidth}" />
-                                                </svg>`
-                                            ),
-                                            top: 0,
-                                            left: 0
-                                        }]);
+                                        svgAnnotations += `<circle cx="${x}" cy="${y}" r="${r}" 
+                                                            fill="none" 
+                                                            stroke="${annotation.stroke || defaultStroke}" 
+                                                            stroke-width="${annotation.lineWidth || defaultLineWidth}" />`;
+                                        if (annotation.label) {
+                                            textX = x - r;
+                                            textY = (y - r - 5 < 0 || y - r - 5 < metadata.height - (y + r + 5 + (annotation.fontSize || defaultFontSize))) ? y + r + 5 + (annotation.fontSize || defaultFontSize) : y - r - 5;
+                                            svgAnnotations += `<text x="${textX}" y="${textY}" font-size="${annotation.fontSize || defaultFontSize}" 
+                                                                fill="${annotation.fontColor || defaultFontColor}" font-family="SourceSansPro">${annotation.label}</text>`;
+                                        }
                                         break;
                                 }
                             });
-                            return image.toBuffer();
+
+                            svgAnnotations += '</svg>';
+
+                            return image.composite([{ input: Buffer.from(svgAnnotations), top: 0, left: 0 }]).toBuffer();
                         })
                         .then(outputBuffer => {
                             msg.payload = outputBuffer;
